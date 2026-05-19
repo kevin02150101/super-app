27865c947358fc59e3f5e37d0287385e67e022d5
kevin02150101 <kkevinchang123@gmail.com>
2026-05-19 08:58:37 +0800

    Add /healthz endpoint for Render health check

--- book/app/controllers/mvc/home_controller.py
+++ book/app/controllers/mvc/home_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify, render_template
 
 home_bp = Blueprint("home", __name__, url_prefix="/")
 
@@ -6,3 +6,8 @@ home_bp = Blueprint("home", __name__, url_prefix="/")
 @home_bp.get("/")
 def index():
     return render_template("home/index.html", title="Book search", active="home")
+
+
+@home_bp.get("/healthz")
+def healthz():
+    return jsonify({"ok": True}), 200
